737df38906de6a637527c6a12ff9c509930c0297
mayankdaruka <53444658+mayankdaruka@users.noreply.github.com>
2020-10-22 00:55:51 -0500

    additional features to ipl table

--- content_script.js
+++ content_script.js
@@ -5,8 +5,17 @@
 //     );
 //  });
 
-document.body.style.backgroundColor = 'yellow'
+document.body.style.backgroundColor = '#c0f3ff'
 
 
 let element = document.querySelector("#sports-app > div > div:nth-child(2) > div > div")
-element.innerHTML = "<div>hello</div>"
+let IPL_elem = document.querySelector("#sports-app > div > div:nth-child(3) > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div")
+if (element)
+   element.innerHTML = element.innerHTML + "<div>This is yonx editing the IPL board for fun!</div>"
+
+if (IPL_elem)
+   // var IPL_elem_attr = IPL_elem.getAttribute('style')
+   // if (!IPL_elem_attr)
+   //    IPL_elem_attr = {}
+   // IPL_elem_attr['background-color'] = 'yellow'
+   IPL_elem.setAttribute('style', "background-color:yellow")
